0cd338eae737d52b4939b2101f6422f0cd25a557
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-30 09:55:06 +0700

    validate input

--- src/GetData.java
+++ src/GetData.java
@@ -1,5 +1,19 @@
 public class GetData {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        if (args.length != 0 && args[0] != null) {
+            int input = 0;
+            try {
+                input = Integer.parseInt(args[0]);
+                if (input > 0 && input < 250){
+                    System.out.println("Correct ID");
+                } else {
+                    System.out.println("Invalid ID");
+                }
+            } catch (NumberFormatException e){
+                System.out.println("Invalid ID");
+            }
+        } else {
+            System.out.println("Invalid Input ");
+        }
     }
 }
